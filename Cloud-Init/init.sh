b91dbe9c0c74beb14fd962086518ca9c7f119beb
KenBarr <ken.barr@rogers.com>
2016-09-13 10:18:49 -0400

    Moving to expect to set up SEMP user

--- Cloud-Init/init.sh
+++ Cloud-Init/init.sh
@@ -7,6 +7,9 @@ export AWS_SECRET_ACCESS_KEY=XXX #<Your AWS Key Secret>
 export AWS_GROUP_ID=XXX #<Your security Policy>
 export AWS_KEY_NAME=XXX #<a TLS PEM you use to access AWS instances>
 export AWS_KEY_VALUE=XXX #<actual content of private key, everything between "BEGIN RSA PRIVATE KEY-----" and "-----END RSA PRIVATE KEY">
+
+export SDKPERF_JAVA=XXX #<The link to download sdkperf_java if required>
+
 export AWS_INSTANCE_TYPE=t2.medium #<t2.medium will be minimum requirement, its not free>
 export AWS_INSTANCE_NAME="AnsibleVMR" #<What ever name you want to see in AWS console>
 export AWS_INSTANCE_AMI=XXX #<AMI instance id for VMR>
@@ -27,7 +30,9 @@ if [ ${VMR_CORE_CLUSTER} == "Y" ]; then
 else
    core_count=1
 fi
+
 export AWS_INSTANCE_COUNT=$((core_count+${VMR_EDGE_NODES}))
+export ANSIBLE_HOST_KEY_CHECKING=False
 
 # Host Setup
 sudo apt-get -y install software-properties-common
@@ -39,6 +44,7 @@ sudo apt-get -y install ansible
 sudo apt-get -y install python-pip
 sudo pip install boto
 sudo pip install awscli
+sudo ansible-galaxy install cmprescott.xml
 
 # Place private key on test hosts
 echo "-----BEGIN RSA PRIVATE KEY-----" > /home/ubuntu/${AWS_KEY_NAME}.pem
@@ -52,18 +58,28 @@ chmod 744 /home/ubuntu/test_env/Tests/*sh
 
 # Create a VMRs
 cd /home/ubuntu/test_env/Ansible
-echo "localhost" > ./hosts
-
+mkdir ./group_vars
+mkdir ./library
+cp /etc/ansible/roles/cmprescott.xml/library/xml ./library
+LOCALHOST_VAR_FILE="./group_vars/LOCALHOST/localhost.yml"
+mkdir ./group_vars/LOCALHOST
+VMRs_VAR_FILE="./group_vars/VMRs/VMRs.yml"
+mkdir ./group_vars/VMRs
+echo "[LOCALHOST]" > ./hosts
+echo "localhost" >> ./hosts
 ansible-playbook -i ./hosts -c local CreateVMR.yml -v
 
 # Configure VMRs
 ansiblePasswd=`date | md5sum | head -c 32`
+echo "---" > ${LOCALHOST_VAR_FILE}
+vmr_core="VMRs_CORE: ["
+vmr_edge="VMRs_EDGE: ["
 echo "" >> ./hosts
 echo "[VMRs]" >> ./hosts
-echo "[VMRs:vars]" >> ./hosts
-echo "ANSIBLE_USERNAME=admin" >> ./hosts
-echo "ANSIBLE_PASSWORD=${ansiblePasswd}" >> ./hosts
-echo "ANSIBLE_SEMP_PORT=8080" >> ./hosts
+echo "---" > ${VMRs_VAR_FILE}
+echo "ANSIBLE_USERNAME: ansibleAdmin" >> ${LOCALHOST_VAR_FILE}
+echo "ANSIBLE_PASSWORD: ${ansiblePasswd}" >>${LOCALHOST_VAR_FILE}
+echo "ANSIBLE_SEMP_PORT: 8080" >> ${LOCALHOST_VAR_FILE}
 echo "[VMRs:children]" >> ./hosts
 echo "VMRs_CORE" >> ./hosts
 echo "VMRs_EDGE" >> ./hosts
@@ -71,20 +87,30 @@ echo "" >> ./hosts
 echo "[VMRs_CORE]" >> ./hosts
 
 count=0
+core="true"
 for file in $( ls VMR* ); do
+   vmr_ip=`grep PRIVATE_IP $file | tr -d PRIVATE_IP=`
+   vmr_name=`grep PRIVATE_DNS $file | tr -d PRIVATE_DNS=`
    if [ ${count} == ${core_count} ]; then
       echo "" >> ./hosts
       echo "[VMRs_EDGE]" >> ./hosts
+      core="false"
    fi
-   echo "`grep PRIVATE_IP $file | tr -d PRIVATE_IP=` ansible_port=2222 ansible_user=sysadmin ansible_ssh_private_key_file=${AWS_KEY_NAME}.pem" >> ./hosts
+   echo "${vmr_ip} ansible_port=2222 ansible_user=sysadmin ansible_ssh_private_key_file=/home/ubuntu/${AWS_KEY_NAME}.pem" >> ./hosts
+   if [ ${core} == "true" ]; then
+      vmr_core="${vmr_core} ${vmr_name}," 
+   else
+      vmr_edge="${vmr_edge} ${vmr_ip},"
+   fi
    count=$((count+1))
 done
+vmr_core="${vmr_core%,}]" # Replace the last "," with "]"
+vmr_edge="${vmr_edge%,}]" # Replace the last "," with "]"
+echo ${vmr_core} >> ${LOCALHOST_VAR_FILE}
+echo ${vmr_edge} >> ${LOCALHOST_VAR_FILE}
 
+#Enable SEMP on all VMRs
 ansible-playbook -i ./hosts EnableSEMP.yml -v
-
-
-
-
 
 
 # Inject sdkperf_java into test environment, using ken.barr@solacesystems.com as marketing token
@@ -92,8 +118,8 @@ sudo apt-get -y install openjdk-7-jre-headless
 sudo apt-get -y install unzip
 mkdir /home/ubuntu/test_env/Sdkperf
 cd /home/ubuntu/test_env/Sdkperf
-wget http://sftp.solacesystems.com/download/SDKPERF_JAVA?mkt_tok=eyJpIjoiWVRZeE1tUm1OamMxTkdRMCIsInQiOiJ6MGZWWGFXOVhzTWdIMmtEamk4R0wrSlNHeHZRbHV5aldKNDNXeTVmdUlRVUl2enNoTEdUaW9LUE1ob1FzUVVDYVUweTNnaFh6dWh3YW1ZM0hVb25BK0s2bXdjQWx4MnU1a1V6dE1EWWVHOD0ifQ%3D%3D
-mv SDKPERF_JAVA\?mkt_tok\=eyJpIjoiWVRZeE1tUm1OamMxTkdRMCIsInQiOiJ6MGZWWGFXOVhzTWdIMmtEamk4R0wrSlNHeHZRbHV5aldKNDNXeTVmdUlRVUl2enNoTEdUaW9LUE1ob1FzUVVDYVUweTNnaFh6dWh3YW1ZM0hVb25BK0s2bXdjQWx4MnU1a1V6dE1EWWVHOD0ifQ%3D%3D  ./sdkperf_java.zip
+wget http://sftp.solacesystems.com/download/${SDKPERF_JAVA}
+mv ${SDKPERF_JAVA}  ./sdkperf_java.zip
 unzip sdkperf_java.zip
 chmod 744 /home/ubuntu/test_env/Sdkperf/*/*sh
 
