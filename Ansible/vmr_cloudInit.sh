dd34e891f69718fc860896025f51bce11ff9e503
KenBarr <KenBarr@github.com>
2016-11-30 17:12:45 -0800

    Wrap admin password set with chmod 600 and 000

--- Ansible/vmr_cloudInit.sh
+++ Ansible/vmr_cloudInit.sh
@@ -4,4 +4,6 @@ passwd=PASSWORD
 newPwd=`openssl passwd -1 ${passwd}`
 newDay=`echo $(($(date --utc --date "$1" +%s)/86400))`
 newPwd2=${newPwd//\//\\/}
+chmod 600 /etc/solace/solace-container.d/shadow
 sed -i '/^admin:/s/!!:[0-9]*/'${newPwd2}':'${newDay}'/' /etc/solace/solace-container.d/shadow
+chmod 000 /etc/solace/solace-container.d/shadow
